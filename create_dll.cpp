8abc1dd41f5d4d94d0dd7057e8cff676ca8559f5
alan4ik <al4ers@gmail.com>
2021-07-07 20:25:05 +0300

    CPP

---
+++ create_dll.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#pragma warning(disable : 4996)
+
+extern "C" {
+    int print_getenv() 
+    { 
+        if (getenv("TEST") != NULL)
+        {
+            printf("get: %s\n", getenv("TEST"));
+            return 101;
+        }
+        else
+        {
+            printf("NO\n");
+            return 404;
+        }
+    }
+}
